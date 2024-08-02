368939e06d28ac3c6a2cbfd8464c44f6e3bf4b27
Florian Hardow <florianhardow@yahoo.de>
2024-08-02 14:38:53 +0200

    chore: setup dc-custom-component template

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='deepset cloud custom component template',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/deepset-ai/dc-custom-component-template',
+    license='MIT',
+    author='Engineering @ deepset cloud',
+    author_email='platform-eng@deepset.ai',
+    description='A template to create custom components for deepset cloud.',
+    install_requires=[
+        # add custom requirements here
+    ],
+)

744a9fb3bd3932d50c4a70704b137bd54270eaa2
Tobias Wochinger <tobias.wochinger@deepset.ai>
2025-01-22 20:13:56 +0100

    chore: clarify about usage

--- src/dc_custom_component/__about__.py
+++ src/dc_custom_component/__about__.py
@@ -1,3 +1,5 @@
 """This file defines the package version."""
 
+# No need to update this is if you are using the GitHub Actions workflow.
+# If the GitHub Actions workflow is used, the version is automatically updated to the given tag.
 __version__ = "0.0.1"
